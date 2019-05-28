b5a0d87d8a43e14cbe32cd4202234833ef76cf93
Colin MacKenzie <colin@flyingeinstein.com>
2019-05-28 16:10:58 -0400

    Changed StringPool to use seperate index so string IDs stay in order. Removed index feature from pool. Added Allocated<> object to pool so allocations return more detail about the memory just allocated.

--- src/Pool.cpp
+++ src/Pool.cpp
@@ -7,13 +7,13 @@
 
 namespace Rest {
 
-    PagedPool::PagedPool(size_t page_size, bool use_index)
-        : _page_size(page_size), _head(nullptr ), _use_index(use_index)
+    PagedPool::PagedPool(size_t page_size)
+        : _page_size(page_size), _head(nullptr )
     {
     }
 
     PagedPool::PagedPool(const PagedPool& copy)
-            : _page_size(copy._page_size), _head(nullptr), _use_index(copy._use_index)
+            : _page_size(copy._page_size), _head(nullptr)
     {
         // copy all pages
         Page *srcp = copy._head;
@@ -25,28 +25,25 @@ namespace Rest {
     }
 
     PagedPool::PagedPool(PagedPool&& move) noexcept
-        : _page_size(move._page_size), _head(move._head), _use_index(move._use_index)
+        : _page_size(move._page_size), _head(move._head)
     {
         move._head = nullptr;
-        move._use_index = false;
     }
 
     PagedPool& PagedPool::operator=(PagedPool&& move) noexcept {
         _page_size = move._page_size;
         _head = move._head;
-        _use_index = move._use_index;
         move._head = nullptr;
-        move._use_index = false;
         return *this;
     }
 
     PagedPool::Page::Page(size_t _size)
-        : _data( (unsigned char*)calloc(1, _size) ), _capacity(_size), _insertp(0), _indexSize(0), _next(nullptr)
+        : _data( (unsigned char*)calloc(1, _size) ), _capacity(_size), _insertp(0), _next(nullptr)
     {
     }
 
     PagedPool::Page::Page(const Page& copy)
-        : _data(nullptr), _capacity(copy._capacity), _insertp(copy._insertp), _indexSize(copy._indexSize), _next(nullptr)
+        : _data(nullptr), _capacity(copy._capacity), _insertp(copy._insertp), _next(nullptr)
         {
         _data = (unsigned char*)calloc(1, _capacity);
         memcpy(_data, copy._data, _capacity);
