c17fe4d4bc72d49d849f88eff98c3705c4087bb3
Colin MacKenzie <colin@flyingeinstein.com>
2020-09-11 21:11:35 -0400

    Rest::Exception now derived from std::exception

--- src/Exception.h
+++ src/Exception.h
@@ -5,6 +5,8 @@
 #ifndef RESTFULLY_EXCEPTION_H
 #define RESTFULLY_EXCEPTION_H
 
+#include <exception>
+
 namespace Rest {
 
     typedef enum {
@@ -17,17 +19,18 @@ namespace Rest {
         UriUnsupportedContentType           = -405,
         Duplicate                           = -406,
         InvalidParameterType                = -501,
-        MissingParameter                    = -502,
-        URL_FAIL_AMBIGUOUS_PARAMETER        = -503,
-        URL_FAIL_EXPECTED_PATH_SEPARATOR    = -504,
-        URL_FAIL_EXPECTED_EOF               = -505,
-        URL_FAIL_INVALID_TYPE               = -506,
-        URL_FAIL_SYNTAX                     = -507,
-        URL_FAIL_INTERNAL                   = -508,
-        URL_FAIL_INTERNAL_BAD_STRING        = -509,
-        URL_FAIL_NULL_ROOT                  = -510,
-        URL_FAIL_EXPECTED_IDENTIFIER        = -511,
-        URL_FAIL_EXPECTED_STRING            = -512
+        ConstParameterType                  = -502,
+        MissingParameter                    = -503,
+        URL_FAIL_AMBIGUOUS_PARAMETER        = -504,
+        URL_FAIL_EXPECTED_PATH_SEPARATOR    = -505,
+        URL_FAIL_EXPECTED_EOF               = -506,
+        URL_FAIL_INVALID_TYPE               = -507,
+        URL_FAIL_SYNTAX                     = -508,
+        URL_FAIL_INTERNAL                   = -509,
+        URL_FAIL_INTERNAL_BAD_STRING        = -510,
+        URL_FAIL_NULL_ROOT                  = -511,
+        URL_FAIL_EXPECTED_IDENTIFIER        = -512,
+        URL_FAIL_EXPECTED_STRING            = -513
     } ParseResult;
 
     /// \brief Convert a return value to a string.
@@ -35,13 +38,15 @@ namespace Rest {
     const char* uri_result_to_string(short result);
 
 
-    class Exception {
+    class Exception : public std::exception {
     public:
         short code;
 
         inline Exception(short _code) : code(_code) {}
 
-        inline const char* message() const { return uri_result_to_string(code); }
+        char const* what() const noexcept override {
+            return uri_result_to_string(code);
+        }
     };
 
 }
